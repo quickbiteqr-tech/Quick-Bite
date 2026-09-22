5c0dbce245e815fded66dfa1aba1f2ef480126a1
Gauhar-1 <mustakkhan1210@gmail.com>
2026-09-22 19:29:02 +0530

    chore: minor UI polishing and dependency syncs

--- tailwind.config.js
+++ tailwind.config.js
@@ -64,6 +64,12 @@ module.exports = {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
+      keyframes: {
+        shine: { 
+          '0%': { backgroundPosition: '200% center' }, 
+          '100%': { backgroundPosition: '0% center' } 
+        }
+      }
     },
   },
   plugins: [],
